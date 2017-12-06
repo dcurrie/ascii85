7854f7872f718e4fe699796f8eea93894ec16d9b
Doug Currie <github.9.eeeeeee@spamgourmet.com>
2017-12-05 21:03:12 -0500

    Add sonar scanner script

---
+++ sonar-scanner.sh
@@ -0,0 +1,7 @@
+sonar-scanner \
+    -Dsonar.projectKey=dcurrie-github-ascii85 \
+    -Dsonar.sources=. \
+    -Dsonar.cfamily.build-wrapper-output=bw-output \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=dcurrie-github \
+    -Dsonar.login=1e24b2dc1670ef1cf875a22457f64cc7e039062b
